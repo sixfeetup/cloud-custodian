3d35eb1bc4dc51721c5bb903a123dbf63e24764b
Daniel Lindsley <daniel@sixfeetup.com>
2025-11-05 10:20:29 -0600

    Revert "Revised tests."
    This reverts commit 05ce84636952212db16ba05aab742973b0129f35.
    
    These tests were meant for a different PR, & somehow snuck in here.

--- tests/terraform/iam_access_key_functional_bad/outputs.tf
+++
@@ -1,8 +0,0 @@
-# Output values for use in tests
-output "test_user_empty_name" {
-  value = aws_iam_user.test_user_empty.name
-}
-
-output "test_user_empty_arn" {
-  value = aws_iam_user.test_user_empty.arn
-}
