a251030ffa75f90b34035ee7ca1efdf7a9ad27b9
Daniel Lindsley <daniel@sixfeetup.com>
2025-12-03 09:29:25 -0600

    Renamed service & resource.

---
+++ c7n/resources/savingsplan.py
@@ -0,0 +1,33 @@
+# Copyright The Cloud Custodian Authors.
+# SPDX-License-Identifier: Apache-2.0
+from c7n.manager import resources
+from c7n.query import QueryResourceManager, TypeInfo
+
+
+@resources.register('savings-plan')
+class SavingsPlan(QueryResourceManager):
+    """AWS SavingsPlans resource.
+
+    Query savings plans for an AWS account.
+
+    :example:
+
+    .. code-block:: yaml
+
+        policies:
+          - name: savings-plans-query
+            resource: savings-plan
+    """
+
+    class resource_type(TypeInfo):
+        service = 'savingsplans'
+        arn_type = 'savingsplan'
+        enum_spec = ('describe_savings_plans', 'SavingsPlans', None)
+        id = 'SavingsPlanId'
+        name = 'SavingsPlanId'
+        config_id = 'savingsPlanId'
+        universal_taggable = True
+        # SavingsPlans are global resources
+        global_resource = True
+        cfn_type = 'AWS::SavingsPlans::SavingsPlan'
+        permissions_augment = ("savingsplans:DescribeSavingsPlans",)
