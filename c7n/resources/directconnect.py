00bda3a26600818304bf3503ded335469890c241
Caleb Syring <caleb.syring@sixfeetup.com>
2025-12-17 13:13:58 -0500

    feat: 10211 aws directconnect add gateway and vif resources

--- c7n/resources/directconnect.py
+++ c7n/resources/directconnect.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 from c7n.manager import resources
 from c7n.query import QueryResourceManager, TypeInfo
-from c7n.tags import universal_augment
 
 
 @resources.register('directconnect')
@@ -17,6 +16,32 @@ class DirectConnect(QueryResourceManager):
         filter_type = 'scalar'
         arn_type = "dxcon"
         universal_taggable = object()
-        permissions_augment = ("directconnect:DescribeTags",)
 
-    augment = universal_augment
+
+@resources.register('directconnect-gateway')
+class DirectConnectGateway(QueryResourceManager):
+
+    class resource_type(TypeInfo):
+        service = 'directconnect'
+        enum_spec = ('describe_direct_connect_gateways', 'directConnectGateways', None)
+        id = 'directConnectGatewayId'
+        name = 'directConnectGatewayName'
+        filter_name = 'directConnectGatewayId'
+        filter_type = 'scalar'
+        arn_type = 'dx-gateway'
+        global_resource = True
+        universal_taggable = object()
+
+
+@resources.register('directconnect-virtual-interface')
+class VirtualInterface(QueryResourceManager):
+
+    class resource_type(TypeInfo):
+        service = 'directconnect'
+        enum_spec = ('describe_virtual_interfaces', 'virtualInterfaces', None)
+        id = 'virtualInterfaceId'
+        name = 'virtualInterfaceName'
+        filter_name = 'virtualInterfaceId'
+        filter_type = 'scalar'
+        arn_type = 'dxvif'
+        universal_taggable = object()
