b6c58027d8f58ae4a6bc2624b9049f6f74343f7c
Daniel Lindsley <daniel@sixfeetup.com>
2026-02-10 11:48:41 -0600

    More lints.

--- tests/terraform/lattice_service_network_association/main.tf
+++ tests/terraform/lattice_service_network_association/main.tf
@@ -4,10 +4,10 @@ resource "random_pet" "network" {
 
 resource "aws_vpclattice_service_network" "test" {
   name = random_pet.network.id
-  
+
   tags = {
     Environment = "Test"
-    ASV = "PolicyTestASV"
+    ASV         = "PolicyTestASV"
   }
 }
 
@@ -18,9 +18,9 @@ data "aws_vpc" "default" {
 resource "aws_vpclattice_service_network_vpc_association" "test" {
   service_network_identifier = aws_vpclattice_service_network.test.id
   vpc_identifier             = data.aws_vpc.default.id
-  
+
   tags = {
     Environment = "Test"
-    ASV = "PolicyTestASV"
+    ASV         = "PolicyTestASV"
   }
 }
