24fb563fa51bab7286825da3eb95ccabaf958d68
Caleb Syring <caleb.syring@sixfeetup.com>
2026-04-17 17:04:52 -0400

    gcp - add set labels for gcp.secret (#10714)

--- tools/c7n_gcp/tests/test_gcp_secretmanager.py
+++ tools/c7n_gcp/tests/test_gcp_secretmanager.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from gcp_common import BaseTest
+from pytest_terraform import terraform
 
 
 class GCPSecretTest(BaseTest):
@@ -17,3 +18,35 @@ class GCPSecretTest(BaseTest):
         self.assertEqual(len(resources), 3)
         self.assertEqual(resources[0]['name'], 'projects/cloud-custodian/'
                                                'secrets/defectdojo_token')
+
+
+@terraform('secret_set_labels')
+def test_secret_set_labels(test, secret_set_labels):
+    secret_name = secret_set_labels['google_secret_manager_secret.secret.name']
+    factory = test.replay_flight_data('gcp-secret-set-label')
+    policy = test.load_policy(
+        {
+            'name': 'gcp-secret-set-label',
+            'resource': 'gcp.secret',
+            'filters': [{
+                'type': 'value',
+                'key': 'name',
+                'value': secret_name,
+            }],
+            'actions': [
+                {
+                    'type': 'set-labels',
+                    'labels': {'env': 'not-the-default'}
+                }
+            ]
+        },
+        session_factory=factory,
+    )
+
+    resources = policy.run()
+    assert len(resources) == 1
+    assert resources[0]['labels']['env'] == 'default'
+
+    client = policy.resource_manager.get_client()
+    result = client.execute_query('get', {'name': secret_name})
+    assert result['labels']['env'] == 'not-the-default'
