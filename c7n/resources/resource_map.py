f2fa1c25768c2a1bb14e0e6487c840d9d1dd3d2f
Rondo <71164236+rondodev@users.noreply.github.com>
2026-09-22 15:02:31 -0400

    adding rds-global-cluster resource (#11092)

--- c7n/resources/resource_map.py
+++ c7n/resources/resource_map.py
@@ -282,6 +282,7 @@ ResourceMap = {
   "aws.rds-cluster-param-group": "c7n.resources.rdsparamgroup.RDSClusterParamGroup",
   "aws.rds-cluster-snapshot": "c7n.resources.rdscluster.RDSClusterSnapshot",
   "aws.rds-db-shard-group": "c7n.resources.rdscluster.RDSDbShardGroup",
+  "aws.rds-global-cluster": "c7n.resources.rdscluster.RDSGlobalCluster",
   "aws.rds-param-group": "c7n.resources.rdsparamgroup.RDSParamGroup",
   "aws.rds-proxy": "c7n.resources.rds.RDSProxy",
   "aws.rds-reserved": "c7n.resources.rds.ReservedRDS",
--- tests/test_policy.py
+++ tests/test_policy.py
@@ -699,7 +699,6 @@ class PolicyMetaLint(BaseTest):
             'AWS::LookoutMetrics::Alert',
             'AWS::MediaPackage::PackagingConfiguration',
             'AWS::MediaPackage::PackagingGroup',
-            'AWS::RDS::GlobalCluster',
             'AWS::RUM::AppMonitor',
             'AWS::ResilienceHub::ResiliencyPolicy',
             'AWS::RoboMaker::RobotApplicationVersion',
